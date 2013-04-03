c62d2489da42efff2e0b674eaa0a66f4dcc3e60f
Jakub Rozmiarek <j.rozmiarek@psp-polska.pl>
2013-04-03 20:13:25 +0200

    initial commit

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '6c5cdbffe91f7c6d4acbe744c2f07084e5ec5ec18e4f9644bbe46071b715112e9c15d5011d6f20e1725bd8c5ee7db3921ee03b55e2925cd99503c447ffc9a6e5'
